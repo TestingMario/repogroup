ca76480f1ce119927280d4f12ec97c2c1374f6c6
maerooteri <ec2-user@ip-172-31-15-2.us-west-1.compute.internal>
2022-08-18 22:48:15 -0400

    tools added

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+ configure git global user's details:
+ ==================================== 
+git config --global user.name "Simon Legah"
+git config --global user.email "legah2000@gmail.com"
+git config --global --list
+
+
+git config --global user.name "maerooteri"
+git commit -m "my first commit"
+git log
